7257449c2fbf4cb3146e973d78823662a47af0d5
Lopes20 <joaogabrielpirs308@gmail.com>
2024-06-14 16:31:17 -0300

    primeiro commit

---
+++ script.js
@@ -0,0 +1,20 @@
+<section class="about" id="about">
+
+        <h1 class="heading">about<span>us</span></h1>
+
+        <div class="row">
+          <div class="image">
+            <img src="img/about-img.jpeg" alt="">
+          </div>
+
+          <div class="content">
+            <h3>what makes our coffee special?</h3>
+            <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. 
+              Labore quidem recusandae iusto magnam pariatur repudiandae alias, 
+              dolor tenetur, nobis minima asperiores sit amet quisquam dolores rerum perferendis repellat quis excepturi.</p>
+              <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. 
+                Accusamus quod ea nostrum itaque placeat adipisci soluta veniam earum aliquid laboriosam.</p>
+                <a href="#" class="botao">Lorem more.</a>
+          </div>
+     </div>
+      </section>
